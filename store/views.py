fcf6d136f9db6c8dc2419a97219a53b25600f0b7
Jacob Crown <jkorona.pl@gmail.com>
2022-03-11 11:41:25 +0100

    add Products view based on Category model

--- store/views.py
+++ store/views.py
@@ -17,3 +17,10 @@ def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
     context = {'product': product}
     return render(request, 'store/products/detail.html', context)
+
+def category_list(request, category_slug):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = category.product.all()
+    context = {'products': products, 'category': category}
+    return render(request, 'store/products/category.html', context)
+    
