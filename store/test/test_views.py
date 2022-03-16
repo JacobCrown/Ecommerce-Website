0cb6b0aee288df38adb1d65084c6f750dc03ac9c
Jacob Crown <jkorona.pl@gmail.com>
2022-03-16 18:25:39 +0100

    add additional test for views

--- store/test/test_views.py
+++ store/test/test_views.py
@@ -6,7 +6,7 @@ from django.test import Client, RequestFactory, TestCase
 from django.urls import reverse
 
 from store.models import Category, Product
-from store.views import all_products
+from store.views import product_all
 
 
 @skip("demonstrating skipping")
@@ -25,7 +25,13 @@ class TestViewResponses(TestCase):
             title="django beginners", created_by_id=1, slug='django-beginners', price=19.99, image='django')
         
     def test_url_allowed_host(self):
-        response = self.c.get('/')
+        response = self.c.get('/', HTTP_HOST='noaddress.com')
+        self.assertEqual(response.status_code, 400)
+        response = self.c.get('/', HTTP_HOST='yourdomain.com')
+        self.assertEqual(response.status_code, 200)
+
+    def test_homepage_url(self):
+        response = self.c.get(reverse('store:product_all'))
         self.assertEqual(response.status_code, 200)
 
     def test_product_detail_url(self):
@@ -42,7 +48,7 @@ class TestViewResponses(TestCase):
 
     def test_homepage_html(self):
         request = HttpRequest()
-        response = all_products(request)
+        response = product_all(request)
         html = response.content.decode('utf8')
         self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
@@ -50,7 +56,7 @@ class TestViewResponses(TestCase):
     
     def test_view_function(self):
         request = self.factory.get('/item/django-beginners')
-        response = all_products(request)
+        response = product_all(request)
         html = response.content.decode('utf8')
         self.assertIn('<title>BookStore</title>', html)
         self.assertTrue(html.startswith('\n<!DOCTYPE html>\n'))
