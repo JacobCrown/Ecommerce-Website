722aa35dbfefe726af16e843881ee94d67b836a6
Jacob Crown <jkorona.pl@gmail.com>
2022-03-17 14:43:09 +0100

    add quantity do the session data and display it on the basket

--- basket/basket.py
+++ basket/basket.py
@@ -6,10 +6,16 @@ class Basket:
             basket = self.session['skey'] = {}
         self.basket = basket
 
-    def add(self, product):
+    def add(self, product, product_qty):
         product_id = product.id
 
         if product_id not in self.basket:
-            self.basket[product_id] = {'price': str(product.price)}
+            self.basket[product_id] = {'price': str(product.price), 'qty': int(product_qty)}
 
         self.session.modified = True
+
+    def __len__(self):
+        """
+        Get the basket data and count the qty od items
+        """
+        return sum(item['qty'] for item in self.basket.values())
--- basket/views.py
+++ basket/views.py
@@ -11,6 +11,8 @@ def basket_add(request):
     basket = Basket(request)
     if request.POST.get('action') == 'post':
         product_id = int(request.POST.get('productid'))
+        product_qty = int(request.POST.get('productqty'))
         product = get_object_or_404(Product, id=product_id)
-        basket.add(product=product)
-        return JsonResponse({'test': 'data'})
+        basket.add(product=product, product_qty=product_qty)
+        basketqty = basket.__len__()
+        return JsonResponse({'qty': basketqty})
